be1ce219210e426b5c200a63652af6754c36f8e3
MrD <the.ubik@gmail.com>
2013-09-01 18:14:23 +0300

    Pre alpha :
    Added monitoring model - does not work :
    http://stackoverflow.com/questions/18419425/java-servlet-project-and-android-library-project-in-eclipse-noclassdeffounderr
    Added jstl

--- src/gr/uoa/di/monitoring/server/servlets/DataCollectionServlet.java
+++ src/gr/uoa/di/monitoring/server/servlets/DataCollectionServlet.java
@@ -1,26 +1,35 @@
 package gr.uoa.di.monitoring.server.servlets;
 
 import java.io.IOException;
+import java.util.Collection;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 @SuppressWarnings("serial")
 @WebServlet("/datacollection")
+@MultipartConfig()
 public final class DataCollectionServlet extends Controller {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		// TODO forbidden !
+		// req.setAttribute("key", Battery.hallo());
 		sc.getRequestDispatcher(DATA_COLLECTION_JSP).forward(req, resp);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-
+		Collection<Part> parts = req.getParts();
+		for (Part part : parts) {
+			req.setAttribute("key", part.getName());
+			sc.getRequestDispatcher(DATA_COLLECTION_JSP).forward(req, resp);
+		}
 	}
 }
